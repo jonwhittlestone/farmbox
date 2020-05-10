2393f6af54d8e5b7e5a72c41e0eec2b3063b5963
Jon Whittlestone <dev@howapped.com>
2020-05-10 15:14:09 +0100

    Add to entrypoint

--- api/entrypoint.prod.sh
+++ api/entrypoint.prod.sh
@@ -3,4 +3,5 @@ pwd
 ls | wc -l
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
+echo "from django.contrib.auth.models import User; u = User.objects.get(username='admin'); u.set_password('${DEFAULT_SUPERUSER_PASSWORD}'); u.save()" | python manage.py shell
 exec "$@"
