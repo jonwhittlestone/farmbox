15957bddb92c1db124dc0d254a5f1856092ff48d
Jon Whittlestone <dev@howapped.com>
2020-05-12 17:52:00 +0100

    Remote backup and restore

--- api/entrypoint.prod.sh
+++ api/entrypoint.prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 pwd
 ls | wc -l
-python manage.py migrate
 python manage.py collectstatic --no-input --clear
-echo "from django.contrib.auth.models import User; u = User.objects.get(username='admin'); u.set_password('${DEFAULT_SUPERUSER_PASSWORD}'); u.save()" | python manage.py shell
+# echo "from django.contrib.auth.models import User; u = User.objects.get(username='admin'); u.set_password('${DEFAULT_SUPERUSER_PASSWORD}'); u.save()" | python manage.py shell
+python manage.py restore_db
 exec "$@"
