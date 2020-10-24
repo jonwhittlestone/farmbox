2684732bfdecb4891aeeb15cd1b1cc0977edcf81
Jon Whittlestone <dev@howapped.com>
2020-10-24 10:00:54 +0100

    sqlite backup

--- api/farmbox/settings/prod.py
+++ api/farmbox/settings/prod.py
@@ -8,6 +8,10 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     },
+    "sqlite": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    },
 }
 
 
